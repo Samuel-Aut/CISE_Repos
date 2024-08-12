3c6340c9db42a5a1ebee35aa9704d144be2443d7
Samuel-Aut <frantickillaace@gmail.com>
2024-08-12 22:39:26 +1200

    worksheet 3 back and front end

---
+++ worksheet3_frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+" use client";
+
+import ShowBookList from "@/components/ShowBookList";
+
+export default function Home() {
+  return (
+    <main>
+            <ShowBookList />   {" "}
+    </main>
+  );
+}
+
+
